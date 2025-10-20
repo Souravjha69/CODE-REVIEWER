ed33dc284f7620c07076154115c5a34ea94966ec
Sourav kumar jha <souravkumarjha@Souravs-MacBook-Air.local>
2025-10-20 03:30:53 -0400

    Some errors resolved of prism js

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,38 @@
-// import { useState } from "react";
+import { useState, useEffect } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import "prismjs/themes/prism-tomorrow.css";
+import prism from "prismjs"; 
 import "./App.css";
 
 function App() {
   // const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    prism.highlightAll();
+  })
   return (
     <main>
       <div className="left">
-        <div className="code"></div>
+        <div className="code">
+          <pre>
+            <code className="language-javascript">
+              {
+                `function sum(){
+                return 1 + 1;}`
+              }
+            </code>
+          </pre>
+        </div>
         <div className="review">Review</div>
       </div>
       <div className="right"></div>
     </main>
   );
+}
+
+function sum() {
+  return 1 + 1;
 }
 
 export default App;
